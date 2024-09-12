0718cd9c759a790b5e6fedaf4b4562b2e5177e2a
KarixD2137 <karol.kopczynski7@gmail.com>
2024-09-12 13:41:33 +0200

    Zadanie 1

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Zadanie1 {
+/*
+Zadanie 1.
+Wypisz na ekranie wszystkie liczby dodatnie dwucyfrowe parzyste. Liczby oddziel znakiem spacji.
+ */
+
+public class Zadanie1 {
+    public void wypiszDwucyfroweDodatnieParzyste()
+    {
+        for (int i = 10; i < 100 ; i=i+2) {
+            System.out.println(i);
+        }
+    }
 }
